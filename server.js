419add2d3bd92f20f0c09cec5f1a39f166780ee9
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-01-23 19:57:09 +0530

    backend done

--- server.js
+++ server.js
@@ -9,29 +9,32 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(
-   cors({
-      origin: "*",
-   })
+  cors({
+    origin: "*",
+  })
 );
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //base route
 app.get("/", (req, res) => {
-   res.status(200).json({ message: "Welcome to the API" });
+  res.status(200).json({ message: "Welcome to the API" });
 });
 
 app.use("/api/v1", router);
 app.use(express.static("./uploads/blogImages"));
 
-app.get("/uploads/blogImages/pho.jpeg", (req, res) => {
-   const imagePath = path.join(__dirname, "/uploads/blogImages/pho.jpeg");
-   res.sendFile(imagePath);
+app.get("/uploads/blogImages/:image", (req, res) => {
+  const imagePath = path.join(
+    __dirname,
+    `/uploads/blogImages/${req.params.image}`
+  );
+  res.sendFile(imagePath);
 });
 
 let PORT = 3008;
 
 app.listen(PORT, async () => {
-   console.log("server started");
-   await connectDb();
-   console.log("database connected");
+  console.log("server started");
+  await connectDb();
+  console.log("database connected");
 });
