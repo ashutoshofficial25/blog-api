535669d429f5f3650aa5d54a1cbf5ba6b6149366
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-01-24 01:12:43 +0530

    refactored

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -3,34 +3,47 @@ const Blog = require("../models/Blog");
 exports.getAllBlogs = async (req, res) => {
   const blogs = await Blog.find();
 
-  if (!blogs.length) {
-    return res.status(200).json({
-      data: [],
+  try {
+    if (!blogs.length) {
+      return res.status(200).json({
+        data: [],
+        message: "No blog posts found",
+      });
+    }
+    res.status(200).json({
+      data: blogs,
+      message: "All blog posts found",
+      total: blogs.length,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: error,
       message: "No blog posts found",
     });
   }
-  res.status(200).json({
-    data: blogs,
-    message: "All blog posts found",
-    total: blogs.length,
-  });
 };
 
 exports.getBlogDetails = async (req, res) => {
   const { id } = req.params;
-  const blog = await Blog.findById(id.toString());
+  try {
+    if (!blog) {
+      return res.status(404).json({
+        data: "null",
+        message: "No blog post found",
+      });
+    }
 
-  if (!blog) {
-    return res.status(404).json({
-      data: "null",
-      message: "No blog post found",
+    res.status(200).json({
+      data: blog,
+      message: "Blog post found",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: error,
+      message: "No blog posts found",
     });
   }
-
-  res.status(200).json({
-    data: blog,
-    message: "Blog post found",
-  });
+  const blog = await Blog.findById(id.toString());
 };
 
 exports.createBlog = async (req, res) => {
