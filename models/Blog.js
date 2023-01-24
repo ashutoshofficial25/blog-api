37d58a0ddc032df9c7fd1ef2c3de1422b3c2721d
Ashutosh Maurya <dev26@bytescrum.com>
2023-01-24 18:19:11 +0530

    blog is working

--- models/Blog.js
+++ models/Blog.js
@@ -1,28 +1,29 @@
 const { default: mongoose } = require("mongoose");
 
 const BlogSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  subtitle: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  thumbnail: {
-    type: String,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+   title: {
+      type: String,
+      required: true,
+   },
+   subtitle: {
+      type: String,
+   },
+   description: {
+      type: String,
+   },
+   thumbnail: {
+      type: String,
+   },
+   content: {
+      type: String,
+      required: true,
+   },
+   date: {
+      type: Date,
+      default: Date.now,
+   },
 });
 
-const Blog = mongoose.model("Blog", BlogSchema);
+const Blog = mongoose.model("blog", BlogSchema);
+
 module.exports = Blog;
