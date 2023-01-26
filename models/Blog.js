138261d5e8859e858c7692e2a74b6c2ffdb124a3
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-01-26 22:19:02 +0530

    blog schema updated

--- models/Blog.js
+++ models/Blog.js
@@ -1,27 +1,30 @@
 const { default: mongoose } = require("mongoose");
 
 const BlogSchema = new mongoose.Schema({
-   title: {
-      type: String,
-      required: true,
-   },
-   subtitle: {
-      type: String,
-   },
-   description: {
-      type: String,
-   },
-   thumbnail: {
-      type: String,
-   },
-   content: {
-      type: String,
-      required: true,
-   },
-   date: {
-      type: Date,
-      default: Date.now,
-   },
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  thumbnail: {
+    type: String,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Blog = mongoose.model("blog", BlogSchema);
